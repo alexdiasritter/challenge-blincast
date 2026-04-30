c2126de498ff51317d8b8d0906a3b8de472be8c3
AlexRitter1 <ritter.alex@hotmail.com>
2026-04-30 15:03:31 -0300

    first commit

---
+++ src/main/java/com/challenge/blincast/BlincastApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.blincast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlincastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlincastApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/challenge/blincast/BlincastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.blincast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlincastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
