6f43d9a19068e0c98bb8a519d95a39b6f42b1817
AlexRitter1 <ritter.alex@hotmail.com>
2026-05-03 14:39:03 -0300

    feature: endidade document

---
+++ src/main/java/com/challenge/blincast/entity/DocumentEntity.java
@@ -0,0 +1,65 @@
+package com.challenge.blincast.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "documents")
+public class DocumentEntity {
+
+    @Id
+    @Column(name = "key", length = 255, nullable = false, unique = true)
+    private String key;
+
+    @Column(name = "value", length = 1000, nullable = false)
+    private String value;
+
+    // Construtor padrão (obrigatório para o JPA/Hibernate)
+    public DocumentEntity() {
+    }
+
+    public DocumentEntity(String key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    // Getters e Setters
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DocumentEntity that = (DocumentEntity) o;
+        return key != null && key.equals(that.key);
+    }
+
+    @Override
+    public int hashCode() {
+        return key != null ? key.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "DocumentEntity{" +
+                "key='" + key + '\'' +
+                ", value='" + value + '\'' +
+                '}';
+    }
+}
